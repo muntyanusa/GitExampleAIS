04669b35c48ebe274ef36a94229cecd2ebb2a6f0
Stanislav Muntyanu <stasean21@yandex.com>
2023-05-26 18:08:10 +0300

    Added length step

--- dfs.py
+++ dfs.py
@@ -1,24 +1,28 @@
 def dfs(graph, start):
     visited = set()  # Множество посещенных вершин
-    stack = [start]  # Стек для обхода
+    stack = [(start, 0)]  # Стек для обхода, включая длину шага
 
     while stack:
-        vertex = stack.pop()  # Извлекаем вершину из стека
+        vertex, distance = stack.pop()  # Извлекаем вершину и длину шага из стека
 
         if vertex not in visited:
             visited.add(vertex)  # Посещаем вершину
-            print(vertex)  # Можно изменить на сохранение в путь
+            print(vertex, distance)  # Можно изменить на сохранение в путь
 
-            # Добавляем непосещенные соседние вершины в стек
-            stack.extend([neighbor for neighbor in graph[vertex] if neighbor not in visited])
+            # Добавляем непосещенные соседние вершины с учетом расстояния в стек
+            neighbors = graph[vertex]
+            for neighbor, step in neighbors:
+                if neighbor not in visited:
+                    stack.append((neighbor, distance + step))
 
-# Пример графа
+# Пример графа с длиной шага между вершинами
 graph = {
-    1: [3],
-    2: [4],
-    3: [1],
-    4: [2]
+    1: [(3, 2)],
+    2: [(4, 1)],
+    3: [],
+    4: [(2, 3)]
 }
 
 # Запуск обхода в глубину
 dfs(graph, 1)
+
